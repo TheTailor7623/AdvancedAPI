3723f7c55f2ebfdfb7bea97e5232a31f6ecab3bc
TheTailor7623 <yseyam7623@gmail.com>
2025-01-27 14:12:35 +0000

    implemented red and green steps in TDD for a function

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 """This is a file used for me to practice testing on"""
+from datetime import date
 
-def addition(x, y):
-    """This function will add x to y and return their sum"""
-    try:
-        if type(x) == str or type(y) == str:
-            x = int(x)
-            y = int(y)
-        sum = x + y
-        return sum
-    except TypeError:
-        raise TypeError("Both inputs must be integers or strings that can be converted to integers.")
+def form(name, surname, dob):
+    """This form will be taking in name, surame and DOB then storing the values in a dictionary with a unique key which will be the id and a list which will contain this data"""
+    current_year = date.today().year
+    user_data = {}
 
-def subtract(x, y):
-    """This function will subtract x and y then return their difference"""
-    difference = x - y
-    return difference
+    if not isinstance(name, str):
+        raise TypeError(f"Name is not a string so it is invalid - the type is {type(name)}")
+    elif not isinstance(surname, str):
+        raise TypeError(f"Surname is not a string so it is invalid - the type is {type(surname)}")
+    elif not isinstance(dob, int):
+        raise TypeError(f"DOB is not an integer so it is invalid - the type is {type(dob)}")
+    elif dob <= (current_year - 150):
+        raise ValueError(f"DOB is more than 150 years old and therefore invalid - with this DOB you would be {current_year - dob} years old!")
+    elif dob >= (current_year):
+        raise ValueError(f"DOB is greater than or equal to the current year therefore invalid - with this DOB you would be {current_year - dob} years old!")
 
-def multiply(x, y):
-    """This function will multiply x and y then return the result"""
-    result = x * y
-    return result
-
-def divide(x, y):
-    """This function will divide x and y then return the result"""
-    result = x / y
-    return result
+    user_data[1] = [name,surname,dob]
+    return user_data
