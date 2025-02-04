c3d1ec56181a3ef7081593a80309c2acded17461
TheTailor7623 <yseyam7623@gmail.com>
2025-02-04 10:44:02 +0000

    Created registration serializer and view as well as simple login view following the beginner REST api course

--- tddPractice/modelsTdd/serializers.py
+++ tddPractice/modelsTdd/serializers.py
@@ -6,3 +6,23 @@ class UserProfileSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
     class Meta:
         model = models.UserProfile
+        fields = ("id","email","name","password")
+        extra_kwargs = {
+            "password":{
+                "write_only":True,
+                "style":{"input_type":"password"}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- tddPractice/modelsTdd/urls.py
+++ tddPractice/modelsTdd/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard.as_view()),
-    # path("profile/", views.profile.as_view()),
-    # path("profile/<int:profile_id>", views.profile.as_view()),
+    path("user/registration/", views.RegisterUserView.as_view()),
+    path("user/login/", views.LoginUserView.as_view()),
 ]
